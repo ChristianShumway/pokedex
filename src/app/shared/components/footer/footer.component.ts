0103be2f51543c2d16cd23fa482fd2d7e76f6346
Christian Torres <cmtorres@minsait.com>
2024-04-24 20:39:51 -0600

    update - maquetación layout, header y footer manejandolo la data en pantalla en el archivo ts

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'shared-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  template: `
+    <footer class="bg-orange-400 shadow dark:bg-gray-800">
+      <div class="w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+        <span class="text-lg text-white dark:text-gray-400">
+          Christian Michelle Torres Martínez
+      </span>
+      </div>
+    </footer>
+  `
 })
-export class FooterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FooterComponent {
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'shared-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  template: `
+    <nav class="bg-orange-400 border-gray-200 dark:border-gray-600 dark:bg-gray-800">
+      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a class="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="./../../../../assets/images/logo.png" class="w-full h-10" alt="Logo sivale" />
+        </a>
+        <div class="text-white dark:text-gray-400">
+          {{ dateNow | date }}
+        </div>
+      </div>
+    </nav>
+  `
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent {
 
   public dateNow = new Date();
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
