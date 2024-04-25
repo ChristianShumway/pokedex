7a8cc70a634342b0f980bc8c5cd8224a02582c2d
Christian Torres <cmtorres@minsait.com>
2024-04-24 19:48:47 -0600

    update - creación de componentes reutilizables en el proyecto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ModalPokemonComponent } from './components/modal-pokemon/modal-pokemon.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ModalPokemonComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    ModalPokemonComponent
   ]
 })
 export class SharedModule { }
